597d2f51eb1fbe495351cd0c5c28c539de2be351
Jan Eike Suchard <jan@suchard.cloud>
2024-08-22 09:27:57 +0200

    changing service name

--- globals/variables.go
+++ globals/variables.go
@@ -9,7 +9,7 @@ import (
 // This file contains globally shared variables (e.g., service name, sql queries)
 
 // ServiceName contains the service's identifying name.
-const ServiceName = "template" //TODO: CHANGE THIS VALUE
+const ServiceName = "user-management"
 
 // SqlQueries contains the prepared sql queries from the resources folder
 var SqlQueries *dotsql.DotSql
