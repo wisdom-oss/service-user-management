796d361e302a0335bcb3f3d01e99fa401cc21bab
Jan Eike Suchard <jan@suchard.cloud>
2024-11-05 17:30:04 +0100

    adding route to get the configuration information from for web clients

--- routes/config.go
+++ routes/config.go
@@ -1,1 +1,20 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"microservice/oidc"
+)
+
+func Configuration(c *gin.Context) {
+	res := struct {
+		ClientID    string `json:"client_id"`
+		Issuer      string `json:"issuer"`
+		RedirectUri string `json:"redirect_uri"`
+	}{
+		ClientID:    oidc.ExternalProvider.ClientID,
+		Issuer:      oidc.ExternalIssuer,
+		RedirectUri: oidc.ExternalProvider.RedirectURL,
+	}
+	c.JSON(200, res)
+}
