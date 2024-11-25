970b1c852c63da436de2c6aba65e0730941df61b
Jan Eike Suchard <jan@suchard.cloud>
2024-11-25 21:00:10 +0100

    fixing errors in client.go

--- types/client.go
+++ types/client.go
@@ -1,17 +1,28 @@
 package types
 
+import (
+	"microservice/resources"
+
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwe"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+)
+
 // Client represents an external app which has been granted permanent access
 // to the WISdoM platform.
 // As the app is always external, it can't be an administrator.
 // Therefore the IsAdministrator function always returns false
 type Client struct {
-	ClientID        string `json:"clientID"`
-	ClientSecret    string `json:"clientSecret"`
-	ApplicationName string `json:"applicationName"`
+	ID      string `json:"clientID" db:"id"`
+	Name    string `json:"name" db:"name"`
+	Contact struct {
+		Name  string `json:"name" db:"contact_name"`
+		EMail string `json:"email" db:"contact_email"`
+	} `json:"contact"`
 }
 
 func (c Client) GetID() string {
-	return c.ClientID
+	return c.ID
 }
 
 func (c Client) Permissions() map[string][]string {
@@ -21,4 +32,23 @@ func (c Client) Permissions() map[string][]string {
 
 func (c Client) IsAdministrator() bool {
 	return false
+}
+
+func (c Client) ReadPermissions(secret string) error {
+	decryptedClientSecret, err := jwe.Decrypt(
+		[]byte(secret),
+		jwe.WithKey(jwa.ECDH_ES, resources.PrivateEncryptionKey),
+	)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Continue development here
+	_, err = jwt.Parse(decryptedClientSecret,
+		jwt.WithIssuer("user-management"),
+		jwt.WithVerify(true),
+		jwt.WithKey(resources.PublicSigningKey.Algorithm(), resources.PublicSigningKey),
+	)
+
+	return nil
 }
