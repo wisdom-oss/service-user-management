2fa4519222347cc25ae4e8d4bbb08959c1dfdec9
Jan Eike Suchard <jan@suchard.cloud>
2024-11-25 18:56:52 +0100

    fixing json marshaling and updating permission generation

--- types/user.go
+++ types/user.go
@@ -24,6 +24,23 @@ func (u User) GetID() string {
 }
 
 func (u User) Permissions() map[string][]string {
+	if u.Administrator {
+		query, err := db.Queries.Raw("get-services")
+		if err != nil {
+			panic(err)
+		}
+		var services []Service
+		err = pgxscan.Select(context.Background(), db.Pool, &services, query)
+		if err != nil {
+			panic(err)
+		}
+
+		permissions := make(map[string][]string)
+		for _, service := range services {
+			permissions[service.Name] = append(permissions[service.Name], service.SupportedScopes...)
+		}
+		return permissions
+	}
 	query, err := db.Queries.Raw("get-user-permissions")
 	if err != nil {
 		return nil
@@ -57,12 +74,24 @@ func (u User) IsActive() bool {
 
 func (u User) MarshalJSON() ([]byte, error) {
 	type output struct {
-		User
-		Permissions map[string][]string `json:"permissions"`
+		ID                 string              `json:"id" db:"id"`
+		ExternalIdentifier string              `json:"externalIdentifier" db:"external_identifier"`
+		Name               string              `json:"name" db:"name"`
+		Email              string              `json:"email" db:"email"`
+		Username           string              `json:"username" db:"username"`
+		Disabled           bool                `json:"disabled" db:"disabled"`
+		Administrator      bool                `json:"administrator" db:"is_admin"`
+		Permissions        map[string][]string `json:"permissions"`
 	}
 	o := output{
-		User:        u,
-		Permissions: u.Permissions(),
+		ID:                 u.ID,
+		ExternalIdentifier: u.ExternalIdentifier,
+		Name:               u.Name,
+		Email:              u.Email,
+		Username:           u.Username,
+		Disabled:           u.Disabled,
+		Administrator:      u.Administrator,
+		Permissions:        u.Permissions(),
 	}
 	return json.Marshal(o)
 }
